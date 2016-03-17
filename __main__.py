5ed422ae843c664824cd23f0b446ada2e9d9f205
Timur Enikeev <te1da@yandex.ru>
2016-03-17 07:42:05 +0300

    Added reconnection if request error

--- __main__.py
+++ __main__.py
@@ -2,8 +2,10 @@
 # -*- coding: utf-8 -*-
 
 from cli import CLI
-
+import logging
+logging.basicConfig(level=logging.WARNING)
 
 if __name__ == "__main__":
     cl = CLI()
 
+logging.shutdown()
--- pveconn.py
+++ pveconn.py
@@ -3,21 +3,35 @@
 import sys, os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import pyproxmox
+import logging
 
 class Pveconn(object):
     """Class for interaction with connection to the Proxmox cluster"""
     def __init__(self, url, username, password):
-        self.login(url, username, password)
-    def login(self, url, username, password):
+        self.url, self.username, self.password = url, username, password
+        self.login()
+    def login(self):
         """Connect to the proxmox cluster"""
-        prox_auth = pyproxmox.prox_auth(url, username, password)
+        prox_auth = pyproxmox.prox_auth(self.url, self.username, self.password)
         pve_conn = pyproxmox.pyproxmox(prox_auth)
         self.conn = pve_conn
+    def reconnect_decorator(function):
+        """Reconnect and retry if exception raised"""
+        def reconnect(self, *args, **kwargs):
+            try:
+                return function(self, *args, **kwargs)
+            except:
+                logging.info("Trying to reconnect")
+                self.login()
+                return function(self, *args, **kwargs)
+        return reconnect
+    @reconnect_decorator
     def node_names(self):
         """Get names of all nodes of the cluster"""
         cs = self.conn.getClusterStatus()['data']
         nodes = [ n['name'] for n in cs if n['type'] == 'node' ]
         return nodes
+    @reconnect_decorator
     def node_vms(self, node):
         """Get a list of qemu VMs of the present node. Format is [{<vmid>:{name:<name>,...}}]"""
         vms_list = self.conn.getNodeVirtualIndex(node)['data']
@@ -25,6 +39,7 @@ class Pveconn(object):
         for vm in vms_list:
             vms_dict[vm['vmid']] = vm
         return vms_dict
+    @reconnect_decorator
     def find_vms(self, request):
         """Find virtual machines with stated text in ID or name"""
         result = {}
@@ -34,17 +49,21 @@ class Pveconn(object):
             if results_of_node:
                 result[node] = results_of_node
         return result
+    @reconnect_decorator
     def get_node_of_vm(self, vmid):
         """Find node where VM is running"""
         for node in self.node_names():
             vms_of_node = self.node_vms(node)
             if vmid in vms_of_node:
                 return node
+    @reconnect_decorator
     def vm_command(self, command, vmid):
         """Execute simple command for stated VM"""
         return getattr(self.conn, "{}VirtualMachine".format(command))(self.get_node_of_vm(vmid), vmid)
+    @reconnect_decorator
     def migrate(self, vmid, new_node, online=False):
         """Migrate VM to another node"""
         return self.conn.migrateVirtualMachine(self.get_node_of_vm(vmid), vmid, new_node, online=True)
+
 
 
